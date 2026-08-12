926b43b59dcde55908c52752c7e106a4eb8fa578
Ebrahim Sabry Farouk <sabryebrahim23@gmail.com>
2026-08-12 15:57:26 +0300

    Day 01 task

---
+++ Day01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace DepiDay01
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Entry point 
+            Console.WriteLine("Hello Depi!");
+            Console.ReadKey();
+        }
+    }
+}
